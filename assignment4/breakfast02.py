946619f680b407db28e8f73a081bbeeec56da4b3
Natthawut26 <108925431+Natthawut26@users.noreply.github.com>
2024-07-05 11:27:38 +0700

    new

--- assignment4/breakfast02.py
+++ assignment4/breakfast02.py
@@ -1,3 +1,28 @@
 # Concurrently breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee():  # Task 1
+    print("coffee: prepare ingredients")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5)  # 2: pause, another tasks can be run
+    print("coffee: ready")
+
+async def fry_eggs():  # Task 1
+    print("eggs: prepare ingredients")
+    sleep(1)
+    print("eggs: frying...")
+    await asyncio.sleep(3)  # 2: pause, another tasks can be run
+    print("eggs: ready")
+
+async def main():  # Task 1
+    start = time()
+    await asyncio.gather(
+        make_coffee(),
+        fry_eggs()
+    )
+    print(f"breakfast is ready in {time()-start} min")
+
+# Run the main function
+asyncio.run(main())
--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -1,3 +1,23 @@
 # Asynchronous breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee():
+    print("Start making coffee")
+    await asyncio.sleep(1)  # Initial step taking 1 second
+    print("Pouring coffee")
+    await asyncio.sleep(5)  # Additional step taking 5 seconds
+    print("Coffee is ready!")
+
+async def fry_eggs():
+    print("Start frying eggs")
+    await asyncio.sleep(1)  # Initial step taking 1 second
+    print("Cooking eggs")
+    await asyncio.sleep(3)  # Additional step taking 3 seconds
+    print("Eggs are ready!")
+
+async def main():
+    await asyncio.gather(make_coffee(), fry_eggs())
+
+# Run the main function
+asyncio.run(main())
