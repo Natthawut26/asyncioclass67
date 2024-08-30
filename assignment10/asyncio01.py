8cf0eb425dd52a11aa806fcb52f167132ca70db5
Natthawut26 <108925431+Natthawut26@users.noreply.github.com>
2024-08-30 10:18:03 +0700

    new

--- assignment10/asyncio01.py
+++ assignment10/asyncio01.py
@@ -1,4 +1,49 @@
-# example of using an asyncio queue
 from random import random
 import asyncio
- 
+import time
+
+# Coroutine to generate work
+async def producer(queue):
+    start_time = time.time()  # Start time for producer
+    print('Producer: Running')
+    # Generate work
+    for i in range(10):
+        # Generate a value
+        value = i
+        # Block to simulate work
+        await asyncio.sleep(random())
+        # Add to the queue
+        print(f'> Producer put {value}')
+        await queue.put(value)
+    # Send an all done signal
+    await queue.put(None)
+    print('Producer: Done')
+    end_time = time.time()  # End time for producer
+    print(f'Producer execution time: {end_time - start_time:.2f} seconds')
+
+# Coroutine to consume work
+async def consumer(queue):
+    start_time = time.time()  # Start time for consumer
+    print('Consumer: Running')
+    # Consume work
+    while True:
+        # Get a unit of work
+        item = await queue.get()
+        # Check for stop signal
+        if item is None:
+            break
+        # Report
+        print(f'\tConsumer got {item}')
+    print('Consumer: Done')
+    end_time = time.time()  # End time for consumer
+    print(f'Consumer execution time: {end_time - start_time:.2f} seconds')
+
+# Entry point coroutine
+async def main():
+    # Create the shared queue
+    queue = asyncio.Queue()
+    # Run the producer and consumers
+    await asyncio.gather(producer(queue), consumer(queue))
+
+# Start the asyncio program
+asyncio.run(main())
